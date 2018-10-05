2f0dd9e76355bf3b120ecfe85fa523817a3524eb
Jon Cooper <joncoop@users.noreply.github.com>
2018-10-05 13:38:13 -0400

    More refactoring, GUI works again

--- zipgrade_reporter.py
+++ zipgrade_reporter.py
@@ -67,6 +67,10 @@ class Scoresheet:
 class Report:
     def __init__(self, scoresheets):
         self.scoresheets = scoresheets
+
+        sort_by = lambda k: k.last_name + " " + k.first_name
+        print(sort_by)
+        self.scoresheets = sorted(self.scoresheets, key=sort_by)
         
     @property
     def versions(self):
@@ -114,6 +118,22 @@ class Report:
 
         return result
     
+    def get_sheets_by_class(self, class_name):
+        result = []
+        for s in self.scoresheets:
+            if s.class_name == class_name:
+                result.append(s)
+                    
+        return result
+
+    def get_sheets_by_version(self, key_version):
+        result = []
+        for s in self.scoresheets:
+            if s.key_version == key_version:
+                result.append(s)
+                    
+        return result
+
     def quartiles(self, num_list):
         nums = num_list.copy()
         nums.sort()
@@ -198,6 +218,89 @@ class Report:
         p.add_run("Min (raw/percent): ")
         p.add_run(str(min_raw) + " / " + str(min_pct) + "%")
 
+    def add_difficulty_analysis(self, document, sheets, version):
+        document.add_heading('Key version: ' + version, 2)
+
+        # {'question': q, 'answer': student_answer, 'correct': correct_answer}
+        misses = {}
+        num_questions = sheets[0].num_questions
+        
+        for s in sheets:
+            for r in s.responses:
+                q = int(r['question'])
+                a = r['answer']
+                c = r['correct']
+                
+                if len(c) > 0:
+                    if q not in misses:
+                        misses[q] = 0
+                        
+                    if a != c:
+                        misses[q] += 1
+
+        difficulty = []
+        for k, v in misses.items():
+            p = round(v / num_questions * 100, 1)
+            difficulty.append((k, v, p))
+
+        sort_by = lambda k: k[1]
+        difficulty = sorted(difficulty, key=sort_by , reverse=True)
+
+        # better idea to use standard deviations for analysis?
+        if len(difficulty) > 10:
+            hard_threshold = difficulty[4][2]
+            easy_threshold = difficulty[-3][2]
+        
+            paragraph = document.add_paragraph("Most difficult Questions (at least " + str(hard_threshold) + "% missed)\n")
+            for d in difficulty:
+                if (d[2] >= hard_threshold):
+                    q = str(d[0])
+                    n = str(d[1])
+                    p = str(d[2])
+                    paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
+
+            paragraph = document.add_paragraph("Easiest Questions (no more than " + str(easy_threshold) + "% missed)\n")
+            for d in difficulty:
+                if (d[2] <= easy_threshold):
+                    q = str(d[0])
+                    n = str(d[1])
+                    p = str(d[2])
+                    paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
+        else:
+            for d in difficulty:
+                q = str(d[0])
+                n = str(d[1])
+                p = str(d[2])
+                paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
+
+    def add_class_summary(self, document, sheets, summary_title=''):
+        if summary_title != '':
+            document.add_heading('Class scores for ' + summary_title, 1)
+        else:
+            document.add_heading('Class scores', 1)
+        
+        table = document.add_table(rows=1, cols=4)
+        table.style = 'Medium Shading 1'
+        
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'Name'
+        hdr_cells[1].text = 'Raw'
+        hdr_cells[2].text = 'Possible'
+        hdr_cells[3].text = 'Percent'
+
+        for s in sheets:
+            row_cells = table.add_row().cells
+            row_cells[0].text = s.last_name + ", " + s.first_name
+            row_cells[1].text = s.earned_points
+            row_cells[2].text = s.possible_points
+            row_cells[3].text = s.percent_correct + "%"
+
+    def add_individual_report_separator(self, document, class_name):
+        paragraph = document.add_paragraph()
+        paragraph.add_run('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        heading = document.add_heading('Individual student reports for\n' + class_name, 1)
+        heading.alignment = 1
+            
     def add_individual_report(self, document, sheet):
         paragraph = document.add_paragraph()
         paragraph.paragraph_format.keep_together = True
@@ -238,7 +341,7 @@ class Report:
     def save(self, document, path=None):
         document.save(path)
         
-    def generate(self, save_path):
+    def generate(self):
         document = docx.Document()
 
         # cover page
@@ -246,51 +349,35 @@ class Report:
         self.add_meta_data(document)
         self.add_summary_statistics(document)
         document.add_page_break()
+        
         # difficulty analysis
+        document.add_heading('Difficulty Analysis', 1)
+        for version in self.versions:
+            sheets = self.get_sheets_by_version(version)
+            self.add_difficulty_analysis(document, sheets, version)
+        document.add_page_break()
         
         # class reports
-
-        # individual reports
-        for s in self.scoresheets:
-            self.add_individual_report(document, s)
+        for class_name in self.classes:
+            sheets = self.get_sheets_by_class(class_name)
+            self.add_class_summary(document, sheets, class_name)
+            document.add_page_break()
             
-        # save (or maybe return document and save there)
-        self.save(document, save_path)
-        
-csv_file_path = '../APCSPTest12Export.csv'
+        # individual reports
+        for i, class_name in enumerate(self.classes):
+            sheets = self.get_sheets_by_class(class_name)
+            self.add_individual_report_separator(document, class_name)
+            document.add_page_break()
+            
+            for s in sheets:
+                self.add_individual_report(document, s)
 
-with open(csv_file_path) as f:
-    lines = f.readlines()
-
-header = lines[0]
-row = lines[1]
-
-sheet_1 = Scoresheet(header, row)
-
-print(sheet_1.quiz_name)
-
-for r in sheet_1.responses:
-    print(r)
-
-all_sheets = []
-
-for line in lines[1:]:
-    sheet = Scoresheet(header, line)
-    all_sheets.append(sheet)
-
-r = Report(all_sheets)
-
-print(r.versions)
-print(r.classes)
-
-save_path = "C:\\Users\\jccooper\\Desktop\\ZipGrade Reporter\\test.docx"
-r.generate(save_path)
+            if i + 1 < len(self.classes):            
+                document.add_page_break()
+            
+        return document
 
 
-
-
-
-"""  
 class App:
     def __init__(self, master):
         self.import_path = None
@@ -347,7 +434,7 @@ class App:
         '''
         pass
     
-    def get_export_filename(self, records):
+    def get_export_filename(self, sheet):
         '''
         Gets path to save report. The report file name is simply the quiz name
         and the export date. If no quiz name exists, then the name will default
@@ -368,21 +455,12 @@ class App:
                   "Oct": "10",
                   "Nov": "11",
                   "Dec": "12"}
-                  
-        r = records[0]
         
-        title = r['QuizName'].strip()
+        title = sheet.quiz_name.strip()
         if len(title) == 0:
-            title = "grade_report"
-
-        '''
-        # can't make this part of file name, a single report might contain multiple classes
-        section = r['QuizClass'].strip()
-        if len(section) == 0:
-            section = ""
-        '''
+            title = "ZipGradeReport"
         
-        date = r['DataExported'].split(" ")
+        date = sheet.date_exported.split(" ")
         yyyy = date[2]
         mm = months[date[0]]
         dd = date[1]
@@ -409,127 +487,13 @@ class App:
                 underscore = True
             
         return filename  + ".docx"
-        
-    def make_difficulty_analysis(self, records, document):
-        '''
-        Generates a list of questions ranked from most to least difficult
-        based on the number of students that miss each question.
-        '''
-
-        document.add_heading('Difficulty Analysis', 1)
-        
-        versions = self.get_versions(records)
-
-        for v in versions:
-            misses = {}
-            r = records[0]
-            num_questions = int((len(r) - 12) / 4) # 12 metadata colums, 4 data cells per answer
-        
-            if len(versions) > 1:
-                document.add_heading('Key version: ' + v, 2)
-                
-            for r in records:
-                if r['KeyVersion'] == v:
-                    for i in range(1, num_questions + 1):
-                        correct = r['Key' + str(i)]
-                        answer = r['Stu' + str(i)]
-                        
-                        if len(correct) > 0:
-                            if i not in misses:
-                                misses[i] = 0
-                                
-                            if answer != correct:
-                                misses[i] += 1
-
-            difficulty = []
-            for k, v in misses.items():
-                p = round(v / num_questions * 100, 1)
-                difficulty.append((k, v, p))
-
-            sort_by = lambda k: k[1]
-            difficulty = sorted(difficulty, key=sort_by , reverse=True)
-
-            # better idea to use standard deviations for analysis?
-            if len(difficulty) > 10:
-                hard_threshold = difficulty[4][2]
-                easy_threshold = difficulty[-3][2]
-            
-                paragraph = document.add_paragraph("Most difficult Questions (at least " + str(hard_threshold) + "% missed)\n")
-                for d in difficulty:
-                    if (d[2] >= hard_threshold):
-                        q = str(d[0])
-                        n = str(d[1])
-                        p = str(d[2])
-                        paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
-
-                paragraph = document.add_paragraph("Easiest Questions (no more than " + str(easy_threshold) + "% missed)\n")
-                for d in difficulty:
-                    if (d[2] <= easy_threshold):
-                        q = str(d[0])
-                        n = str(d[1])
-                        p = str(d[2])
-                        paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
-            else:
-                for d in difficulty:
-                    q = str(d[0])
-                    n = str(d[1])
-                    p = str(d[2])
-                    paragraph.add_run("\tq=" + q + ", n=" + n + ", %=" + p + "\n")
-                    
-        document.add_page_break()
                                            
-        
-    
-    def make_score_summary(self, records, document):
-        '''
-        Creates summary of indidual student scores.
-        '''
-        classes = self.get_classes(records)
-        
-        for c in classes:
-            if c != '':
-                document.add_heading('Class scores for ' + c, 1)
-            else:
-                document.add_heading('Class scores', 1)
-            
-            table = document.add_table(rows=1, cols=4)
-            table.style = 'Medium Shading 1'
-            
-            hdr_cells = table.rows[0].cells
-            hdr_cells[0].text = 'Name'
-            hdr_cells[1].text = 'Raw'
-            hdr_cells[2].text = 'Possible'
-            hdr_cells[3].text = 'Percent'
-
-            for r in records:
-                if c == r['QuizClass']:
-                    row_cells = table.add_row().cells
-                    row_cells[0].text = r['LastName'] + ", " + r['FirstName']
-                    row_cells[1].text = r['EarnedPts']
-                    row_cells[2].text = r['PossiblePts']
-                    row_cells[3].text = r['PercentCorrect'] + "%"
-                
-            document.add_page_break()
-
-
-
-    def save(self, document, records):
-        '''
-        Creates Word Doc with individual score reports.
-        '''                            
+    def save(self, document):
         try:
-            self.save_path = self.export_path + "/" + self.get_export_filename(records)
             document.save(self.save_path)
             self.status_lbl_text.set("Your report is ready!")
         except:
             self.status_lbl_text.set("Unable to save report. Check file and disk permissions.")
-
-    def make_student_report_cover(self, class_name, document):
-        paragraph = document.add_paragraph()
-        paragraph.add_run('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
-        heading = document.add_heading('Individual student Reports for\n' + class_name, 1)
-        heading.alignment = 1
-        document.add_page_break()
         
     def generate(self):
         '''
@@ -538,40 +502,55 @@ class App:
         generated = False
         
         if self.import_path != None:
-            try:
-                records = self.csv_to_json(self.import_path)
+                #try:
+                with open(self.import_path) as f:
+                    lines = f.readlines()
 
-                document = docx.Document()
-                self.make_cover_page(records, document)
-                self.make_difficulty_analysis(records, document)
-                self.make_score_summary(records, document)
+                header = lines[0]
+                all_sheets = []
+
+                for line in lines[1:]:
+                    sheet = Scoresheet(header, line)
+                    all_sheets.append(sheet)
+
+                r = Report(all_sheets)
+                self.save_path = self.export_path + '/' + self.get_export_filename(all_sheets[0])
                 
-                classes = self.get_classes(records)
+                document = r.generate()
                 
-                for i, c in enumerate(classes):
-                    if c != '':
-                        self.make_student_report_cover(c, document)
-                        
-                    for r in records:
-                        if c == r['QuizClass']:
-                            self.make_individual_report(r, document)
-
-                    if i < len(classes) - 1:
-                        document.add_page_break()
-
-                generated = True
-            except Exception as inst:
-                print(inst)
+                    
+                #except Exception as inst:
+                #print(inst)
                 self.status_lbl_text.set("Something went wrong. Be sure your CSV data file is valid.")
 
-            if generated:
-                self.save(document, records)
+                #if generated:
+                self.save(document)
         else:
             self.status_lbl_text.set("You must select a file first!")
 
+"""    
+csv_file_path = '../APCSPTest12Export.csv'
+
+with open(csv_file_path) as f:
+    lines = f.readlines()
+
+header = lines[0]
+
+all_sheets = []
+
+for line in lines[1:]:
+    sheet = Scoresheet(header, line)
+    all_sheets.append(sheet)
+
+r = Report(all_sheets)
+
+save_path = "C:\\Users\\jccooper\\Desktop\\ZipGrade Reporter\\test.docx"
+r.generate(save_path)
+
+print("done!")
+"""
 
 root = Tk()
 #root.iconbitmap('assets/my_icon.ico')
 my_gui = App(root)
 root.mainloop()
-"""
